eaaeb046c3a7a5a48f21b6f6b31fb324ee377846
RyanBarnettbuff <rdbarnett1@buffs.wtamu.edu>
2026-01-26 16:19:54 -0600

    Add files via upload

---
+++ Homework1.cs
@@ -0,0 +1,14 @@
+﻿namespace Homework1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // homework1 code
+        double X=2.5;
+        double Y=3.3;
+        double Z=(4*X*X) +(3*Y); 
+
+        Console.WriteLine(Z);
+    }
+}

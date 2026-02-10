d982cc8ecd8f72a261456a8a157572498887b67f
RyanBarnettbuff <rdbarnett1@buffs.wtamu.edu>
2026-02-09 20:23:41 -0600

    Homework 3 Ryan Barnett Submitted

---
+++ Homework3.cs
@@ -0,0 +1,84 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Homework3;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Q1 
+        
+        Console.WriteLine("Please input a number: ");
+        int N = int.Parse(Console.ReadLine());
+
+        if (N < 2)
+        {
+            Console.WriteLine($"{N} is not prime!");
+        }
+
+        bool primeCheck=true;
+        int number=2;
+
+        while (number < N)
+        {
+            if (N % number == 0)
+            {
+                primeCheck=false;
+                break;
+            }
+
+            number++;
+
+        }
+
+        if (primeCheck)
+        {
+            Console.WriteLine($"{N} is prime!");
+        }
+
+        else
+        {
+            Console.WriteLine($"{N} is not prime!");
+        }
+
+
+        //Q2
+
+        
+        Console.WriteLine("Please input a number: ");
+        int N = int.Parse(Console.ReadLine());
+
+        for(int row=1; row<=N; row++)
+        {
+            for(int column=1; column<=(N-1); column++)
+            {
+                Console.Write("#");
+            }
+            Console.WriteLine("#");
+
+        }
+
+
+        //Q3
+
+        Console.WriteLine("Please input a number: ");
+        int N = int.Parse(Console.ReadLine());
+
+        for(int row=1; row<=N; row++)
+        {
+            for(int column=1; column<row; column++)
+            {
+                Console.Write("#");
+            }
+            Console.WriteLine("#");
+
+        }
+
+
+
+
+
+
+    }
+}
